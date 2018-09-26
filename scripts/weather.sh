25b9ed7807d48f067315dd9093125a8158951d2c
Chris Vick <cvick@users.noreply.github.com>
2018-09-25 20:04:22 -0700

    added new line character after the output

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -12,4 +12,4 @@ if [ -z $1 ]; then
     exit 0;
 fi
 
-curl --connect-timeout 30 -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${FAHRENHEIT}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+curl --connect-timeout 30 -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${FAHRENHEIT}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1 \n"; }'
